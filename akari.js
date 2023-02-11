e2c3c651ac19155fc96e8c7275c95bfd8e1ec543
寺嶋朱里 <renkon0327@gmail.com>
2023-02-11 14:08:58 +0900

    変更A

--- akari.js
+++ akari.js
@@ -1,19 +1,20 @@
-var navbar = document.querySelector(".navbar");
-// console.log(navbar);
+$(function () {
+    $(window).on("scroll", function () {
+        if (100 < $(this).scrollTop()) {
+            $(".header").addClass("change-color");
+        } else {
+            $(".header").removeClass("change-color");
+        }
+    })
+    })
 
-var menu = document.querySelector(".menu");
-console.log(menu);
 
-// ヘッダーがULタグの位置に来たらヘッダーを固定する。
-window.addEventListener("scroll", function () {
-    // console.log(window.pageYOffset);
-    // console.log(menu.offsetTop);
-    if (window.pageYOffset >= menu.offsetTop) {
-        navbar.classList.add("sticky");
-    } else {
-        navbar.classList.remove("sticky");
-    }
-});
+$("#hamburger").on("click", function () {
+    $(".icon").toggleClass("close");
+    $(".sm").slideToggle();
+})
+
+
 
 let index = 0;
 let slideMax = $('.slide').length
